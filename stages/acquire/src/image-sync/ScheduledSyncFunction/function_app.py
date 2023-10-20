d3dca227376945a3790eaae928deaa177d03a856
Toddy Mladenov <toddysm@gmail.com>
2023-10-20 15:57:10 -0700

    Removed the tag to tuple function and added event creation one

--- stages/acquire/src/image-sync/ScheduledSyncFunction/function_app.py
+++ stages/acquire/src/image-sync/ScheduledSyncFunction/function_app.py
@@ -45,7 +45,7 @@ def ScheduledImageSyncFunction(myTimer: func.TimerRequest) -> None:
                 if digest not in sync_tag['digests']:
                     sync_tag['digests'].append(digest)
                     # TODO: Maybe convert the tag to JSON
-                    publish_eventgrid_event("ImageSync", f"{config['registry']}/{config['repository']}", tag_tuple_to_json(tag))
+                    publish_eventgrid_event("ImageSync", f"{config['registry']}/{config['repository']}", create_event(config["registry"], config["repository"], tag))
             else:
                 logging.info(f"Tag {sync_tag['name']} not found.")
 
@@ -84,9 +84,11 @@ def publish_eventgrid_event(event_type, subject, data):
         ]
     )
 
-def tag_tuple_to_json(tuple):
-    tag_tuple = {
-        "name": tuple[0],
-        "digest": tuple[1]
+def create_event(registry, repository, tag):
+    event = {
+        "registry": registry,
+        "repository": repository,
+        "name": tag[0],
+        "digest": tag[1]
     }
-    return json.dumps(tag_tuple)
+    return json.dumps(event)
