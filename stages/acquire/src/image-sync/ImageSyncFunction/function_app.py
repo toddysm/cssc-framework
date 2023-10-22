211104c03a525e232898438aeedc9e0366b85b10
Toddy Mladenov <toddysm@gmail.com>
2023-10-21 18:04:41 -0700

    Added log statement to trace the events

--- stages/acquire/src/image-sync/ImageSyncFunction/function_app.py
+++ stages/acquire/src/image-sync/ImageSyncFunction/function_app.py
@@ -28,6 +28,7 @@ def ImageSyncFunction(event: func.EventGridEvent):
     #                                                    "2019-06-01-preview")
     
     result = event.get_json()
+    logging.info(f"Event data: {result}")
      
     # Create a task run request object
     task_values = [
