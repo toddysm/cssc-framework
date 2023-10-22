c2cf7b29f0095a8066eaa5c792162c9dd784313d
Toddy Mladenov <toddysm@gmail.com>
2023-10-21 18:42:15 -0700

    Added more debugging logs

--- stages/acquire/src/image-sync/ImageSyncFunction/function_app.py
+++ stages/acquire/src/image-sync/ImageSyncFunction/function_app.py
@@ -28,6 +28,7 @@ def ImageSyncFunction(event: func.EventGridEvent):
     #                                                    "2019-06-01-preview")
     
     result = event.get_json()
+    logging.info(f"Event type: {type(event)}")
     result = json.loads(result)
     logging.info(f"Event data: {result} \n Result type: {type(result)}")
 
