7238cbdc398631f9e4bb42fb8d59f466eb09f7ad
Toddy Mladenov <toddysm@gmail.com>
2023-10-23 15:46:06 -0700

    Updated test script to create scan task for acquisition
    Signed-off-by: Toddy Mladenov <toddysm@gmail.com>

--- tests/manual/image-sync/create-tasks.sh
+++ tests/manual/image-sync/create-tasks.sh
@@ -1,6 +1,7 @@
 ACR_NAME=tsmacrquarantineusw2
 ACR_TASK_NAME_COPY_IMAGE=acquire-copy-image-task
 ACR_TASK_NAME_GENERATE_SBOM=acquire-generate-sbom-task
+ACR_TASK_NAME_SCAN_IMAGE=acquire-scan-image-task
 
 # Create acquire-copy-image-task in ACR
 az acr task create -r $ACR_NAME -n $ACR_TASK_NAME_COPY_IMAGE \
@@ -12,6 +13,13 @@ az acr task create -r $ACR_NAME -n $ACR_TASK_NAME_COPY_IMAGE \
 # Create acquire-generate-sbom-task in ACR
 az acr task create -r $ACR_NAME -n $ACR_TASK_NAME_GENERATE_SBOM \
     -f ../../../stages/acquire/src/image-sync/generate-sbom-task.yaml \
+    -c /dev/null \
+    --commit-trigger-enabled false \
+    --base-image-trigger-enabled false
+
+# Create acquire-scan-image-task in ACR
+az acr task create -r $ACR_NAME -n $ACR_TASK_NAME_SCAN_IMAGE \
+    -f ../../../stages/acquire/src/image-sync/scan-and-patch-image-task.yaml \
     -c /dev/null \
     --commit-trigger-enabled false \
     --base-image-trigger-enabled false
