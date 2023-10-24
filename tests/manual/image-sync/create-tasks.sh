b4dac7e664d2531597ed8d47df09ac74384ae10a
Toddy Mladenov <toddysm@gmail.com>
2023-10-24 10:47:28 -0700

    Updated manual test script with sign task
    Signed-off-by: Toddy Mladenov <toddysm@gmail.com>

--- tests/manual/image-sync/create-tasks.sh
+++ tests/manual/image-sync/create-tasks.sh
@@ -2,6 +2,7 @@ ACR_NAME=tsmacrquarantineusw2
 ACR_TASK_NAME_COPY_IMAGE=acquire-copy-image-task
 ACR_TASK_NAME_GENERATE_SBOM=acquire-generate-sbom-task
 ACR_TASK_NAME_SCAN_IMAGE=acquire-scan-image-task
+ACR_TASK_NAME_SIGN_IMAGE=acquire-sign-image-task
 
 # Create acquire-copy-image-task in ACR
 az acr task create -r $ACR_NAME -n $ACR_TASK_NAME_COPY_IMAGE \
@@ -20,6 +21,13 @@ az acr task create -r $ACR_NAME -n $ACR_TASK_NAME_GENERATE_SBOM \
 # Create acquire-scan-image-task in ACR
 az acr task create -r $ACR_NAME -n $ACR_TASK_NAME_SCAN_IMAGE \
     -f ../../../stages/acquire/src/image-sync/scan-and-patch-image-task.yaml \
+    -c /dev/null \
+    --commit-trigger-enabled false \
+    --base-image-trigger-enabled false
+
+# Create acquire-sign-image-task in ACR
+az acr task create -r $ACR_NAME -n $ACR_TASK_NAME_SIGN_IMAGE \
+    -f ../../../stages/acquire/src/image-sync/sign-image-task.yaml \
     -c /dev/null \
     --commit-trigger-enabled false \
     --base-image-trigger-enabled false
