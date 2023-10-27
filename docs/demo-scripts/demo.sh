27dc2b3016f68edce1b8abcc112892da989bb542
Toddy Mladenov <toddysm@gmail.com>
2023-10-27 15:56:03 -0700

    Updated demo script
    Signed-off-by: Toddy Mladenov <toddysm@gmail.com>

--- docs/demo-scripts/demo.sh
+++ docs/demo-scripts/demo.sh
@@ -1,12 +1,40 @@
+# NOTE: The commands below use sudo because they are for Ubuntu VM on Azure
 
+# Pull the images
+sudo docker pull python:3.12.0-slim
+sudo docker pull python:3.11.6-slim
+sudo docker pull python:3.11.5-slim
+sudo docker pull python:3.10.13-slim
+sudo docker pull python:3.10.12-slim
+
+sudo docker images
+
+# Retag the images
+sudo docker image tag python:3.12.0-slim toddysm/python:3.12
+#sudo docker image tag python:3.11.6-slim toddysm/python:3.11.6-slim
+sudo docker image tag python:3.11.5-slim toddysm/python:3.11
+#sudo docker image tag python:3.10.13-slim toddysm/python:3.10.13-slim
+sudo docker image tag python:3.10.12-slim toddysm/python:3.10
+
+# Sign into Docker Hub
+sudo docker login -u toddysm
+
+# Push the images to Docker Hub
+sudo docker push toddysm/python:3.12
+sudo docker push toddysm/python:3.11
+sudo docker push toddysm/python:3.10
+
+# Sign into Azure Container Registry
+sudo az login --use-device-code
+sudo az acr login --name tsmacrquarantineusw2
 
 # Trivy commands
 trivy image --vuln-type os --ignore-unfixed python:3.12.0-slim
 trivy image --vuln-type os --ignore-unfixed python:3.11.6-slim
 trivy image --vuln-type os --ignore-unfixed python:3.11.5-slim
 trivy image --vuln-type os --ignore-unfixed python:3.10.13-slim
-trivy image --vuln-type os --ignore-unfixed python:3.10.12-slim | grep Total
-trivy image --vuln-type os --ignore-unfixed python:3.10.12-slim-patched | grep Total
+trivy image --vuln-type os --ignore-unfixed tsmacrquarantineusw2.azurecr.io/python:3.10.12-slim | grep Total
+trivy image --vuln-type os --ignore-unfixed tsmacrquarantineusw2.azurecr.io/python:3.10.12-slim-patched | grep Total
 
 
 # Copa commands
