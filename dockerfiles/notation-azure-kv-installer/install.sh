276ba719d853f6b986539e76a57debc840a7b8d1
Toddy Mladenov <toddysm@gmail.com>
2026-06-01 20:43:00 -0700

    chore: reset repo content for custos demo
    Remove all subfolder content (dockerfiles, docs, sample, stages, tests) and
    .github content except the workflows directory. Delete build-app.yaml so the
    repository can be reorganized as a demo for the custos project workflows.

--- dockerfiles/notation-azure-kv-installer/install.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-TARGET_FOLDER="$HOME/.config/notation/plugins/azure-kv"
-TARGET_PATH="$TARGET_FOLDER/notation-azure-kv"
-
-echo "Installing notation-azure-kv plugin..."
-echo "Target path: $TARGET_PATH"
-mkdir -p $TARGET_FOLDER
-cp /resources/notation-azure-kv $TARGET_PATH
